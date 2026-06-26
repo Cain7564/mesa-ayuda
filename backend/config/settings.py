935e4ab90fdb74914984567a4839e6be09fa124b
Cain7564 <156337459+Cain7564@users.noreply.github.com>
2026-06-26 12:10:51 -0600

    database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -95,16 +95,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-
-        'NAME': config('DB_NAME'),
-
-        'USER': config('DB_USER'),
-
-        'PASSWORD': config('DB_PASSWORD'),
-
-        'HOST': config('DB_HOST'),
-
-        'PORT': config('DB_PORT'),
+        'NAME': 'mesa_ayuda',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -132,7 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'America/Guatemala'
+TIME_ZONE = 'America/EL'
 
 USE_I18N = True
 
