4f746f1adb774aff9db09f74549323913f55391b
Cain7564 <156337459+Cain7564@users.noreply.github.com>
2026-06-30 11:40:18 -0600

    tickets

--- backend/tickets/admin.py
+++ backend/tickets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Categoria, SLA, Ticket, Seguimiento
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(SLA)
+admin.site.register(Ticket)
+admin.site.register(Seguimiento)
