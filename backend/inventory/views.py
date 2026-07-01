99cd7323343b29fefd3efa5528cf0cb628794738
Cain7564 <156337459+Cain7564@users.noreply.github.com>
2026-07-01 10:40:06 -0600

    views

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import (
+    Marca,
+    SistemaOperativo,
+    Ubicacion,
+    EstadoEquipo,
+    Equipo,
+)
+
+from .serializers import (
+    MarcaSerializer,
+    SistemaOperativoSerializer,
+    UbicacionSerializer,
+    EstadoEquipoSerializer,
+    EquipoSerializer,
+)
+
+
+class MarcaListCreateView(generics.ListCreateAPIView):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+
+class MarcaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+
+class SistemaOperativoListCreateView(generics.ListCreateAPIView):
+    queryset = SistemaOperativo.objects.all()
+    serializer_class = SistemaOperativoSerializer
+
+
+class SistemaOperativoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SistemaOperativo.objects.all()
+    serializer_class = SistemaOperativoSerializer
+
+
+class UbicacionListCreateView(generics.ListCreateAPIView):
+    queryset = Ubicacion.objects.all()
+    serializer_class = UbicacionSerializer
+
+
+class UbicacionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ubicacion.objects.all()
+    serializer_class = UbicacionSerializer
+
+
+class EstadoEquipoListCreateView(generics.ListCreateAPIView):
+    queryset = EstadoEquipo.objects.all()
+    serializer_class = EstadoEquipoSerializer
+
+
+class EstadoEquipoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = EstadoEquipo.objects.all()
+    serializer_class = EstadoEquipoSerializer
+
+
+class EquipoListCreateView(generics.ListCreateAPIView):
+    queryset = Equipo.objects.all()
+    serializer_class = EquipoSerializer
+
+
+class EquipoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Equipo.objects.all()
+    serializer_class = EquipoSerializer
