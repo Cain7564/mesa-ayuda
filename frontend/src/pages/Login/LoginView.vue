906d20536afa0e11cf130d2824e014c2cd0fdc19
Cain7564 <156337459+Cain7564@users.noreply.github.com>
2026-07-02 07:57:22 -0600

    login

---
+++ frontend/src/pages/Login/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <div>
+
+    <h1>Mesa de Ayuda</h1>
+
+    <h2>Iniciar Sesión</h2>
+
+  </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import LoginView from '../pages/Login/LoginView.vue'
+
 const routes = [
+
+  {
+    path: '/',
+    redirect: '/login'
+  },
+
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  }
 
 ]
 
 const router = createRouter({
 
-    history: createWebHistory(),
+  history: createWebHistory(),
 
-    routes,
+  routes,
 
 })
 
