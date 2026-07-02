d8ceb18fcf91052b726e4f7195bc25354c6957d0
Cain7564 <156337459+Cain7564@users.noreply.github.com>
2026-07-02 08:17:05 -0600

    dashboard

---
+++ frontend/src/pages/Dashboard/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="dashboard">
+
+    <h1>Dashboard</h1>
+
+    <h2>Bienvenido al Sistema Mesa de Ayuda</h2>
+
+  </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.dashboard{
+
+    padding:40px;
+
+}
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import LoginView from '../pages/Login/LoginView.vue'
-
+import DashboardView from '../pages/Dashboard/DashboardView.vue'
 const routes = [
 
   {
-    path: '/',
-    redirect: '/login'
+    path:'/',
+    redirect:'/login'
   },
 
   {
-    path: '/login',
-    name: 'login',
-    component: LoginView
+    path:'/login',
+    name:'login',
+    component:LoginView
+  },
+
+  {
+    path:'/dashboard',
+    name:'dashboard',
+    component:DashboardView
   }
 
 ]
