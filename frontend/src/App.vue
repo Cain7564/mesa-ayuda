b887cc915d225253199c2e73d58e15ae1b2dc82b
Cain7564 <156337459+Cain7564@users.noreply.github.com>
2026-07-02 07:57:41 -0600

    app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,25 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+
+  <RouterView />
+
 </template>
 
-<style scoped></style>
+<script setup>
+
+import { RouterView } from 'vue-router'
+
+</script>
+
+<style>
+
+body {
+
+  margin: 0;
+
+  font-family: Arial, Helvetica, sans-serif;
+
+  background-color: #f5f5f5;
+
+}
+
+</style>
